5722cef6e33f15018b56c595bc28c8b0dd964e8f
fatih.altuntas <fatih.altuntas@trendyol.com>
2020-10-23 20:21:16 +0300

    Add run script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-PORT=4000 iex --name node-1@0.0.0.0 -S mix phx.server &
-PORT=4001 iex --name node-2@0.0.0.0 -S mix phx.server & 
-PORT=4002 iex --name node-3@0.0.0.0 -S mix phx.server & 
+nohup $(PORT=4000 iex --name node-1@0.0.0.0 -S mix phx.server > node-1.log )&
+nohup $(PORT=4001 iex --name node-2@0.0.0.0 -S mix phx.server > node-2.log )&
+PORT=4002 iex --name node-3@0.0.0.0 -S mix phx.server
